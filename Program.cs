becbaf858d2f399634c52da095170572172c5879
AnastasiaShumilenko <119425257+AnastasiaShumilenko@users.noreply.github.com>
2022-12-10 23:52:11 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace task92
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int[] myArray = Enumerable.Range(1,8).ToArray();
+
+           for (int i=0; i<myArray.Length; i++ )
+           {
+            Console.WriteLine(myArray[i]);
+           }
+        }
+
+    }
+}
